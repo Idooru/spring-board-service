7702683a645ba50e3b6983fc70c25e092ebf5f6a
Idooru <shere1765@gmail.com>
2022-08-21 23:49:07 +0900

    Clone coding board service with spring boot

---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,39 @@
+package com.study.board.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    private String content;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Board board = (Board) o;
+        return id != null && Objects.equals(id, board.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
